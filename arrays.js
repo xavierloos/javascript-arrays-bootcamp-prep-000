f57e357b1d89aad7c9726d8f2047e40209bb999d
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-06-03 15:17:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,14 +32,13 @@ function addElementToEndOfArray(array, element){
 }     
 
 //8) arrays destructivelyAddElementToEndOfArray(array, element) adds an element to the end of an array:
-/*function destructivelyAddElementToEndOfArray(array, element){
-  return [...array, element]
-}    */ 
-
-//9) arrays destructivelyAddElementToEndOfArray(array, element) alters the original array:
 function destructivelyAddElementToEndOfArray(array, element){
-  return array.push(element)
-}     
+  array.push(element)
+    return array
+    return array.push(element)
+}    
+
+     
  
 //10) arrays accessElementInArray(array, index) accesses the element in array at the given index:
 function accessElementInArray(array, index){
@@ -47,16 +46,11 @@ function accessElementInArray(array, index){
 }    
     
 //11) arrays destructivelyRemoveElementFromBeginningOfArray(array) returns the array with the first element removed:
-/*function  destructivelyRemoveElementFromBeginningOfArray(array){
-  return array.slice(1)
-  
-} */   
-
-//12) arrays destructivelyRemoveElementFromBeginningOfArray(array) alters the original array:
 function  destructivelyRemoveElementFromBeginningOfArray(array){
-  return array.shift()
-  array
-}    
+  array.shift()
+  return array
+  
+}   
 
 //13) arrays removeElementFromBeginningOfArray(array) removes the first element from the array:
 function  removeElementFromBeginningOfArray(array){
@@ -70,23 +64,21 @@ function  removeElementFromBeginningOfArray(array){
 }  
 
 //15) arrays destructivelyRemoveElementFromEndOfArray(array) returns the array with the last element removed:
-/*function destructivelyRemoveElementFromEndOfArray(array){
-  return array.slice(0, array.length -1)
-}  */
-
-//16) arrays destructivelyRemoveElementFromEndOfArray(array) alters the original array:
 function destructivelyRemoveElementFromEndOfArray(array){
-  return array.pop() 
-  array
-}
+  array.slice(0, array.length - 1)
+  array.pop() 
+  return array
+} 
+
+
 //====================
 //17) arrays removeElementFromEndOfArray(array) removes the last element from the array:
-/*function removeElementFromEndOfArray(array) {
-  return array.pop()
+function removeElementFromEndOfArray(array) {
   
-}*/
+ return array.slice(0, array.length - 1)
 
-//18) arrays removeElementFromEndOfArray(array) does not alter the original array:
- function removeElementFromEndOfArray(array) {
-  return array.slice(-1)
+  
+  
+  
 }
+
